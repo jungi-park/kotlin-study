51889c18b9b1290e2354537707df569a54de58c0
박준기 <qmqqqqm@gmail.com>
2024-05-10 17:48:02 +0900

    코틀린-스프링-api호출-입문 2강 완료

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,47 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
+//    var 는 읽기/쓰기 가능
+    var i: Int = 10
+//    val 은 읽기만 가능
+    val j: Int = 10
+
+    i = 20
+//    j = 20
+
+    println(i)
+    println(j)
+
+//    타입 뒤에 ? 를 붙여야 null 사용가능
+    var a: Int = 10
+    var b: Int? = 10
+
+//    a =null
+    b = null
+
+    println(a)
+    println(b)
+
+    var k: String = "ABC"
+    var l: String? ="ABC"
+
+//    k = null
+    l = null
+
+    println(k)
+    println(l)
+
+//    타입추론 -> 변수에 들어오는 값을 보고 타입을 알아서 지정해줌
+    val z = "ABC"
+    val x = 1
+    val c = 1L
+    val v = 1.0
+    val n = 1.0f
+
+    println("z = " + z::class)
+    println("x = " + x::class)
+    println("c = " + c::class)
+    println("v = " + v::class)
+    println("n = " + n::class)
 }
